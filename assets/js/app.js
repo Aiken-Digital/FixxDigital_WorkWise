4ce2d21edf553854c293d246f4f8eba4566c35d8
Ahmad Rifaii <ariffma@gmail.com>
2016-03-17 22:18:29 +0800

    singlepage.html load more function

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,16 @@ if ($("aside.article-share").length > 0) {
 	});
 };
 
+var urlPerPage = 9;
+
+$(window).resize(function() {
+	if ($( window ).width() < 922) {
+		urlPerPage = 3;
+	} else {
+		urlPerPage = 9;
+	};
+});
+
 $( document ).ready(function() { 
 
 	new WOW().init();
@@ -122,6 +132,109 @@ $( document ).ready(function() {
 				$specialSlider.addClass("two");
 			}
 		},
+	});
+
+
+
+
+
+	var urlPage = 1;
+	var urlString = '';
+	var urlStringQoutes = '';
+
+	var chosenQoute = [];
+
+	$('.loadmore p').click(function(){
+		if ($( window ).width() < 922) {
+			urlPerPage = 3;
+		} else {
+			urlPerPage = 9;
+		};
+
+		urlPage += 1;
+		urlString = '/articles?per_page='+urlPerPage+'&page='+urlPage+'&category=employers';
+		urlStringQoutes = '/quotes';
+
+		$.ajax({
+			// url: urlStringQoutes
+			url: 'assets/js/app_data_qoutes.json',
+			type: 'GET',
+			dataType: "json",
+			
+			success: function(data, textStatus, jqXHR) {
+				var newQoute = '';
+
+				var qoutes = data["Response"]["Rows"];
+				var chosen = qoutes[Math.floor(Math.random()*qoutes.length)];
+
+				var qouteText = chosen["Text"];
+				var qouteType = chosen["Type"];
+				var qouteImage = chosen["Image"];
+				var qouteVideo = chosen["VideoId"];
+
+				newQoute = "<div class='row dotted qoutes-"+qouteType+" toggle wow fadeInDown'> <div class='col-md-8 col-md-offset-2 col-xs-12'> <p> <span> “ </span> <span class='text'>"+qouteText+"</span> <span> ” </span> </p> </div> </div> ";
+
+				if (qouteType == "image") {
+					newQoute = "<div class='row dotted qoutes-"+qouteType+" toggle wow fadeInDown'> <div class='col-md-4 col-md-offset-2 col-xs-12'> <figure> <img src='"+qouteImage+"'> </figure> </div> <div class='col-md-5 col-xs-12 col-xs-offset-0'> <p class='title'> <span>"+qouteText+"</span> </p> </div> </div> ";
+				};
+
+				if (qouteType == "video") {
+					newQoute = "<div class='row dotted qoutes-"+qouteType+" toggle wow fadeInDown'> <div class='col-md-4 col-md-offset-2 col-xs-12'> <figure> <img src='"+qouteImage+"'> <span class='overlay'> <a href='https://www.youtube.com/embed/"+qouteVideo+"?wmode=transparent'></a> </span> </figure> </div> <div class='col-md-5 col-xs-12 col-xs-offset-0'> <p class='title'> <span>"+qouteText+"</span> </p> </div> </div> ";
+				};
+
+				$(".divider .container").html(newQoute);
+				$(".categories .container").append(newQoute);
+			},
+
+			error: function() {
+				console.log('failed');
+			},
+		});
+
+		$.ajax({
+			// url: urlString
+			url: 'assets/js/app_data.json',
+			type: 'GET',
+			dataType: "json",
+			
+			success: function(data, textStatus, jqXHR) {
+				var appendRowArcticle = '';
+				for (var i = 0; i < urlPerPage; i++) {
+					var title = data["Response"]["Rows"][i]["Title"];
+					var date = data["Response"]["Rows"][i]["Date"];
+					var type = data["Response"]["Rows"][i]["Type"];
+					var image = data["Response"]["Rows"][i]["Image"];
+					var desc = data["Response"]["Rows"][i]["Description"];
+					var url = data["Response"]["Rows"][i]["ArticleUrl"];
+
+					var appendArcticle = "<div class='col-md-4 col-xs-12 col-sm-12'> <article> <figure class='"+type+"'> <img src='"+image+"'> <span> <a href='#category-label'> "+type+" </a> </span> </figure> <time datetime='2015-11-27' pubdate> "+date+" </time> <header> <a href='"+url+"'>"+title+"</a> </header> <p>"+desc+"</p> </article> </div>";
+					
+					if (type == "video") {
+						appendArcticle = "<div class='col-md-4 col-xs-12 col-sm-12'> <article> <figure class='"+type+"'> <img src='"+image+"'> <span> <a href='#category-label'> "+type+" </a> </span> <span class='overlay'> <a href='"+url+"'> </a> </span> </figure> <time datetime='2015-11-27' pubdate> "+date+" </time> <header> <a href='"+url+"'>"+title+"</a> </header> <p>"+desc+"</p> </article> </div>";
+					};
+
+					// opening row tag
+					if (i%3 == 0) {
+						appendRowArcticle += "<div class='row content wow fadeInDown'>"+appendArcticle;
+					}
+
+					// closing row tag
+					else if (i%3 == 2) {
+						appendRowArcticle += appendArcticle+"</div>";
+					}
+
+					else {
+						appendRowArcticle += appendArcticle;
+					}
+				};
+
+				$(".categories .container").append(appendRowArcticle);
+			},
+
+			error: function() {
+				console.log('failed');
+			},
+		});
 	});
 
 
